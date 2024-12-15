fdd831fdbea0aea836ee42d5bc01c50eb46d0e7f
Grimmauld <Grimmauld@grimmauld.de>
2024-12-15 22:34:50 +0100

    WIP: add overlay and module

--- flake.nix
+++ flake.nix
@@ -18,24 +18,34 @@
       nix-github-actions,
       flake-utils,
     }:
+    let
+      gen_shared = pkgs: import ./nix/aa-shared.nix { inherit (pkgs) python3 lib; };
+      gen_aa_pkgs =
+        pkgs:
+        let
+          shared = gen_shared pkgs;
+          aa_pkgs = with pkgs; {
+            apparmor-src = callPackage ./pkgs/apparmor-sources.nix { inherit shared; }; # support
+            aa-teardown = callPackage ./pkgs/aa-teardown.nix { inherit aa_pkgs; }; # support
+            apparmor-bin-utils = callPackage ./pkgs/apparmor-bin-utils.nix { inherit aa_pkgs shared; };
+            apparmor-kernel-patches = callPackage ./pkgs/apparmor-kernel-patches.nix {
+              inherit aa_pkgs shared;
+            };
+            apparmor-pam = callPackage ./pkgs/apparmor-pam.nix { inherit aa_pkgs shared; };
+            apparmor-parser = callPackage ./pkgs/apparmor-parser.nix { inherit aa_pkgs shared; };
+            apparmor-profiles = callPackage ./pkgs/apparmor-profiles.nix { inherit aa_pkgs shared; };
+            apparmor-utils = callPackage ./pkgs/apparmor-utils.nix { inherit aa_pkgs shared; };
+            libapparmor = callPackage ./pkgs/libapparmor.nix { inherit aa_pkgs shared; };
+          };
+        in
+        aa_pkgs;
+    in
     (flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        shared = import ./nix/aa-shared.nix { inherit (pkgs) python3 lib; };
-        aa_pkgs = with pkgs; {
-          apparmor-src = callPackage ./pkgs/apparmor-sources.nix { inherit shared; }; # support
-          aa-teardown = callPackage ./pkgs/aa-teardown.nix { inherit aa_pkgs; }; # support
-          apparmor-bin-utils = callPackage ./pkgs/apparmor-bin-utils.nix { inherit aa_pkgs shared; };
-          apparmor-kernel-patches = callPackage ./pkgs/apparmor-kernel-patches.nix {
-            inherit aa_pkgs shared;
-          };
-          apparmor-pam = callPackage ./pkgs/apparmor-pam.nix { inherit aa_pkgs shared; };
-          apparmor-parser = callPackage ./pkgs/apparmor-parser.nix { inherit aa_pkgs shared; };
-          apparmor-profiles = callPackage ./pkgs/apparmor-profiles.nix { inherit aa_pkgs shared; };
-          apparmor-utils = callPackage ./pkgs/apparmor-utils.nix { inherit aa_pkgs shared; };
-          libapparmor = callPackage ./pkgs/libapparmor.nix { inherit aa_pkgs shared; };
-        };
+        shared = gen_shared pkgs;
+        aa_pkgs = gen_aa_pkgs pkgs;
         check_pkgs = with pkgs; {
           regression-test-src = callPackage ./check/regression-test-src.nix {
             inherit aa_pkgs shared;
@@ -55,9 +65,20 @@
         formatter = pkgs.nixfmt-rfc-style;
       }
     ))
-    // {
+    // rec {
+      overlays = {
+        default = final: gen_aa_pkgs;
+      };
+
       githubActions = nix-github-actions.lib.mkGithubMatrix {
         checks = nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks;
       }; # todo: figure out testing on aarch64-linux
+
+      nixosModules = {
+        # Use a locally built Lix
+        default = {
+          nixpkgs.overlays = [ overlays.default ];
+        };
+      };
     };
 }
